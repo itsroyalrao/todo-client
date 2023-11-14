40bc4e77e8e73d0ceb91cdabc42aecdb235e2dd0
Mohit Yadav <my701319@gmail.com>
2023-11-14 20:22:35 +0530

    seventh commit

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <>
       <div className="bg-gradient-to-br from-blue-600 to-blue-400 min-h-screen flex flex-col items-center justify-center">
-        <div className="w-[80vw] h-[80vh] sm:w-[400px] sm:h-[450px] bg-white rounded-lg sm:rounded-3xl flex flex-col items-center px-4 relative">
+        <div className="w-[100%] h-[100vh] sm:w-[400px] sm:h-[450px] bg-white sm:rounded-3xl flex flex-col items-center px-4 relative">
           <div className="font-bold text-3xl text-gray-800 py-2">Todo</div>
           <div className="w-full py-4 flex text-lg">
             <input type="checkbox" className="sm:cursor-pointer" />
@@ -67,7 +67,7 @@ export default function Home() {
               </div>
             </div>
           </div>
-          <div className="w-[95%] bg-blue-600 flex justify-center absolute bottom-3 text-white p-2 rounded sm:rounded-xl">
+          <div className="w-[95%] bg-blue-600 flex justify-center absolute bottom-3 text-white p-2 rounded sm:rounded-xl sm:cursor-pointer">
             Add Todo
           </div>
         </div>
