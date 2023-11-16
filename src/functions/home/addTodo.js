6ec6bb2782d8ddf6dae1a988ad278c7a4501585a
Mohit Yadav <my701319@gmail.com>
2023-11-16 18:20:57 +0530

    twenty-fourth commit

--- src/functions/home/addTodo.js
+++ src/functions/home/addTodo.js
@@ -10,10 +10,13 @@ export default async function addTodo(
 ) {
   if (inputValue) {
     try {
+      const email = localStorage.getItem("user");
+
       await axios.post(
         // "http://localhost:3000/todo",
         "https://todos-app.up.railway.app/todo",
         {
+          email,
           inputValue,
         }
       );
--- src/functions/home/getTodos.js
+++ src/functions/home/getTodos.js
@@ -2,8 +2,12 @@ import axios from "axios";
 
 export default async function getTodos(setTodos) {
   try {
-    // const response = await axios.get("http://localhost:3000/todo");
-    const response = await axios.get("https://todos-app.up.railway.app/todo");
+    const email = localStorage.getItem("user");
+
+    // const response = await axios.get(`http://localhost:3000/todo?email=${email}`);
+    const response = await axios.get(
+      `https://todos-app.up.railway.app/todo?email=${email}`
+    );
     setTodos(response.data.data);
   } catch (e) {
     console.log(e.message);
