f73be0ce4c8243d4c549b6919ace5dad1681af48
Mohit Yadav <my701319@gmail.com>
2023-12-20 13:58:22 +0530

    33th commit

--- src/functions/backendUrl.js
+++ src/functions/backendUrl.js
@@ -1,4 +1,4 @@
-const url = () => "http://localhost:3000";
-// const url = () => "https://todo-dwiq.onrender.com";
+// const url = () => "http://localhost:3000";
+const url = () => "https://todo-dwiq.onrender.com";
 
 export { url };
